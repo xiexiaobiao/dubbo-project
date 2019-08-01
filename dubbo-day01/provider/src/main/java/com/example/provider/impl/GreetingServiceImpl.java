51eec0fc758d3d05d0a11f6166d5f30d761e9b8c
xiexiaobiao <kool.biao@qq.com>
2019-08-01 20:11:06 +0800

    day01-final

---
+++ dubbo-day01/provider/src/main/java/com/example/provider/impl/GreetingServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.provider.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.example.common.api.GreetingService;
+import com.example.common.constant.AnnotationConstants;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+/**
+ * @Classname ProviderImpl
+ * @Description
+ * @Author xiexiaobiao
+ * @Date 2019-07-31 17:39
+ * @Version 1.0
+ **/
+
+//一定注意这里的service类别
+@Service(version = AnnotationConstants.VERSION)
+public class GreetingServiceImpl implements GreetingService, Serializable {
+
+    @Override
+    public String greeting(String name) {
+        System.out.println("provider received invoke of greeting: " + name);
+        sleepWhile();
+        return "Annotation, greeting " + name;
+    }
+
+    private void sleepWhile() {
+
+    }
+}
