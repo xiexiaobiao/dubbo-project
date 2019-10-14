dbffabc5005a061f532052591740616fb2508c38
xiexiaobiao <kool.biao@qq.com>
2019-10-14 23:24:44 +0800

    day18

--- dubbo-day18/storage/src/main/java/com/biao/mall/storage/impl/ProductServiceImpl.java
+++ dubbo-day18/storage/src/main/java/com/biao/mall/storage/impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.biao.mall.storage.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.biao.mall.common.dto.CommodityDTO;
 import com.biao.mall.storage.dao.ProductDao;
@@ -19,25 +20,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductServiceImpl extends ServiceImpl<ProductDao, ProductEntity> implements ProductService {
 
-/*    @Override
-    public ObjectResponse decreaseStorage(CommodityDTO commodityDTO) {
-        int storage = baseMapper.decreaseStorage(commodityDTO.getCommodityCode(), commodityDTO.getCount());
-        ObjectResponse<Object> response = new ObjectResponse<>();
-        if (storage > 0){
-            response.setStatus(RspStatusEnum.SUCCESS.getCode());
-            response.setMessage(RspStatusEnum.SUCCESS.getMessage());
-            return response;
-        }
-        response.setStatus(RspStatusEnum.FAIL.getCode());
-        response.setMessage(RspStatusEnum.FAIL.getMessage());
-        return response;
-    }*/
-
     @Override
     public boolean prepare(BusinessActionContext actionContext, CommodityDTO commodityDTO) {
         System.out.println("actionContext获取Xid prepare>>> "+actionContext.getXid());
         System.out.println("actionContext获取TCC参数 prepare>>> "+actionContext.getActionContext("commodityDTO"));
         int storage = baseMapper.decreaseStorage(commodityDTO.getCommodityCode(), commodityDTO.getCount());
+        //测试rollback时打开
+        /*int a = 1/0;
+        System.out.println(a);*/
         if (storage > 0){
             return true;
         }
@@ -53,12 +43,13 @@ public class ProductServiceImpl extends ServiceImpl<ProductDao, ProductEntity> i
     @Override
     public boolean rollback(BusinessActionContext actionContext) {
         System.out.println("actionContext获取Xid rollback>>> "+actionContext.getXid());
-        CommodityDTO commodityDTO = (CommodityDTO) actionContext.getActionContext("commodityDTO");
+        //必须注意actionContext.getActionContext返回的是Object,且不可使用以下语句直接强转！
+        //CommodityDTO commodityDTO = (CommodityDTO) actionContext.getActionContext("commodityDTO");
+        CommodityDTO commodityDTO = JSONObject.toJavaObject((JSONObject)actionContext.getActionContext("commodityDTO"),CommodityDTO.class);
         int storage = baseMapper.increaseStorage(commodityDTO.getCommodityCode(), commodityDTO.getCount());
         if (storage > 0){
             return true;
         }
         return false;
     }
-
 }
